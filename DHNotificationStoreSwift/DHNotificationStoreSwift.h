453e807b6578a1e521fe2e33ca6f8d19436f147c
Dave Hardiman <dave.hardiman@ordnancesurvey.co.uk>
2015-12-01 09:09:17 +0000

    Initial commit

---
+++ DHNotificationStoreSwift/DHNotificationStoreSwift.h
@@ -0,0 +1,19 @@
+//
+//  DHNotificationStoreSwift.h
+//  DHNotificationStoreSwift
+//
+//  Created by Dave Hardiman on 01/12/2015.
+//  Copyright © 2015 David Hardiman. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DHNotificationStoreSwift.
+FOUNDATION_EXPORT double DHNotificationStoreSwiftVersionNumber;
+
+//! Project version string for DHNotificationStoreSwift.
+FOUNDATION_EXPORT const unsigned char DHNotificationStoreSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DHNotificationStoreSwift/PublicHeader.h>
+
+
---
+++ DHNotificationStoreSwift/NotificationHandler.swift
@@ -0,0 +1,14 @@
+//
+//  NotificationHandler.swift
+//  DHNotificationStoreSwift
+//
+//  Created by Dave Hardiman on 01/12/2015.
+//  Copyright © 2015 David Hardiman. All rights reserved.
+//
+
+import Foundation
+
+/// Protocol to conform to to gain DHNotificationStore functionality
+protocol NotificationHandler: class {
+
+}
